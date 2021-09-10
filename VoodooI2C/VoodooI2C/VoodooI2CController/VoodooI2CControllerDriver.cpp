73a4d61af67181264f86c2da3f0d99714d71870d
williambj1 <45396585+williambj1@users.noreply.github.com>
2021-09-10 11:56:11 +1000

    Actually use bus timing config defaults where necessary

--- VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CControllerDriver.cpp
+++ VoodooI2C/VoodooI2C/VoodooI2CController/VoodooI2CControllerDriver.cpp
@@ -28,11 +28,18 @@ IOReturn VoodooI2CControllerDriver::getBusConfig() {
     bus_device.transaction_fifo_depth = 32;
     bus_device.receive_fifo_depth = 32;
 
-    if (nub->getACPIParams((const char*)"SSCN", &bus_device.acpi_config.ss_hcnt, &bus_device.acpi_config.ss_lcnt, NULL) != kIOReturnSuccess)
+    if (nub->getACPIParams((const char*)"SSCN", &bus_device.acpi_config.ss_hcnt, &bus_device.acpi_config.ss_lcnt, NULL) != kIOReturnSuccess) {
+        bus_device.acpi_config.ss_hcnt = 0x01b0;
+        bus_device.acpi_config.ss_lcnt = 0x01fb;
         error = true;
+    }
 
-    if (nub->getACPIParams((const char*)"FMCN", &bus_device.acpi_config.fs_hcnt, &bus_device.acpi_config.fs_lcnt, &bus_device.acpi_config.sda_hold) != kIOReturnSuccess)
+    if (nub->getACPIParams((const char*)"FMCN", &bus_device.acpi_config.fs_hcnt, &bus_device.acpi_config.fs_lcnt, &bus_device.acpi_config.sda_hold) != kIOReturnSuccess) {
+        bus_device.acpi_config.fs_hcnt = 0x48;
+        bus_device.acpi_config.fs_lcnt = 0xa0;
+        bus_device.acpi_config.sda_hold = 0x9;
         error = true;
+    }
 
     if (error)
         return kIOReturnNotFound;
@@ -466,11 +473,6 @@ bool VoodooI2CControllerDriver::start(IOService* provider) {
 
     if (getBusConfig() != kIOReturnSuccess) {
         IOLog("%s::%s Warning: Error getting bus config, using defaults where necessary\n", getName(), bus_device.name);
-        bus_device.acpi_config.ss_hcnt = 0x01b0;
-        bus_device.acpi_config.fs_hcnt = 0x48;
-        bus_device.acpi_config.ss_lcnt = 0x01fb;
-        bus_device.acpi_config.fs_lcnt = 0xa0;
-        bus_device.acpi_config.sda_hold = 0x9;
     } else {
         IOLog("%s::%s Got bus configuration values\n", getName(), bus_device.name);
     }
